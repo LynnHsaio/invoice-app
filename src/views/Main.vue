59e31015d6f70e0d5f4a23be75049f69d1b405b6
Lilin <ihsetheyme@gmail.com>
2023-08-05 13:43:39 +0800

    [IMP] 主頁加入最上方標題及按鈕

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="Main page">
     <div class="container">
-      <header class="top">
-        <h1 class="heading">Invoices</h1>
-        <p class="text-small color-purple">There are {{ 7 }} total invoices</p>
-      </header>
+      <div class="toolbar">
+        <header>
+          <h1 class="heading">Invoices</h1>
+          <p class="text-small color-purple">
+            There are {{ 7 }} total invoices
+          </p>
+        </header>
+        <div class="btns">
+          <button class="btn btn--white">
+            <span> Filter by status </span>
+            <img src="../assets/icon-arrow-down.svg" alt="arrow down icon" />
+          </button>
+          <button class="btn btn-new btn--purple">
+            <img src="../assets/icon-plus.svg" alt="puls icon" />
+            <span> New Invoice</span>
+          </button>
+        </div>
+      </div>
       <main></main>
     </div>
   </div>
@@ -15,4 +29,10 @@ export default {
   components: {},
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.toolbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
